774f86af020ad81dae7c39730f789edbc1f80522
DariuszPelkaPL <dariuszpelka88@gazeta.pl>
2019-06-18 07:28:28 +0200

    Finsihed unit test for main monthly calculation function

--- XUnitTestForMerchantFeeCalculationEngineProject/MerchantFeeCalculationUnitTests.cs
+++ XUnitTestForMerchantFeeCalculationEngineProject/MerchantFeeCalculationUnitTests.cs
@@ -68,8 +68,8 @@ namespace XUnitTestForMerchantFeeCalculationEngineProject
             // Arrange
             var merchants = new Dictionary<string, Merchant>();
             merchants.Add("7-ELEVEN", new Merchant() { DiscountPercentage = 0, Name = sevenEleven, FeeAsPercentage = 1 });
-            merchants.Add("CIRCLE_K", new Merchant() { DiscountPercentage = 0, Name = circleK, FeeAsPercentage = 1 });
-            merchants.Add("TELIA", new Merchant() { DiscountPercentage = 0, Name = telia, FeeAsPercentage = 1 });
+            merchants.Add("CIRCLE_K", new Merchant() { DiscountPercentage = 20, Name = circleK, FeeAsPercentage = 1 });
+            merchants.Add("TELIA", new Merchant() { DiscountPercentage = 10, Name = telia, FeeAsPercentage = 1 });
             merchants.Add("NETTO", new Merchant() { DiscountPercentage = 0, Name = netto, FeeAsPercentage = 1 });
 
             List<Transaction> transactions = new List<Transaction>
@@ -106,9 +106,73 @@ namespace XUnitTestForMerchantFeeCalculationEngineProject
 
             // Assert
             Assert.Equal(transactions.Count, processedTransactions.Count);
+
+            // Month #9
             Assert.Equal(30, processedTransactions[0].Fee);
             Assert.Equal(sevenEleven, processedTransactions[0].RelatedTransaction.Owner.Name);
             Assert.Equal(new DateTime(2018, 9, 1), processedTransactions[0].RelatedTransaction.DoneOn);
+
+            Assert.Equal(29.8M, processedTransactions[1].Fee);
+            Assert.Equal(circleK, processedTransactions[1].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 4), processedTransactions[1].RelatedTransaction.DoneOn);
+
+            Assert.Equal(29.9M, processedTransactions[2].Fee);
+            Assert.Equal(telia, processedTransactions[2].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 7), processedTransactions[2].RelatedTransaction.DoneOn);
+
+            Assert.Equal(30, processedTransactions[3].Fee);
+            Assert.Equal(netto, processedTransactions[3].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 9), processedTransactions[3].RelatedTransaction.DoneOn);
+
+            Assert.Equal(0.8M, processedTransactions[4].Fee);
+            Assert.Equal(circleK, processedTransactions[4].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 13), processedTransactions[4].RelatedTransaction.DoneOn);
+
+            Assert.Equal(0.9M, processedTransactions[5].Fee);
+            Assert.Equal(telia, processedTransactions[5].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 16), processedTransactions[5].RelatedTransaction.DoneOn);
+
+            Assert.Equal(1M, processedTransactions[6].Fee);
+            Assert.Equal(sevenEleven, processedTransactions[6].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 19), processedTransactions[6].RelatedTransaction.DoneOn);
+
+            Assert.Equal(0.8M, processedTransactions[7].Fee);
+            Assert.Equal(circleK, processedTransactions[7].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 22), processedTransactions[7].RelatedTransaction.DoneOn);
+
+            // Month #10
+            Assert.Equal(30, processedTransactions[11].Fee);
+            Assert.Equal(sevenEleven, processedTransactions[11].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 1), processedTransactions[11].RelatedTransaction.DoneOn);
+
+            Assert.Equal(29.8M, processedTransactions[12].Fee);
+            Assert.Equal(circleK, processedTransactions[12].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 4), processedTransactions[12].RelatedTransaction.DoneOn);
+
+            Assert.Equal(29.9M, processedTransactions[13].Fee);
+            Assert.Equal(telia, processedTransactions[13].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 7), processedTransactions[13].RelatedTransaction.DoneOn);
+
+            Assert.Equal(30, processedTransactions[14].Fee);
+            Assert.Equal(netto, processedTransactions[14].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 10), processedTransactions[14].RelatedTransaction.DoneOn);
+
+            Assert.Equal(0.8M, processedTransactions[15].Fee);
+            Assert.Equal(circleK, processedTransactions[15].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 13), processedTransactions[15].RelatedTransaction.DoneOn);
+
+            Assert.Equal(0.9M, processedTransactions[16].Fee);
+            Assert.Equal(telia, processedTransactions[16].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 16), processedTransactions[16].RelatedTransaction.DoneOn);
+
+            Assert.Equal(1M, processedTransactions[17].Fee);
+            Assert.Equal(sevenEleven, processedTransactions[17].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 19), processedTransactions[17].RelatedTransaction.DoneOn);
+
+            Assert.Equal(0.8M, processedTransactions[18].Fee);
+            Assert.Equal(circleK, processedTransactions[18].RelatedTransaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 10, 22), processedTransactions[18].RelatedTransaction.DoneOn);
+
         }
     }
 }
