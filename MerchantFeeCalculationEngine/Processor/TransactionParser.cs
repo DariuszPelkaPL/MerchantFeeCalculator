c33b9b2e89ea1de87c19120d30d00b250e49cf93
DariuszPelkaPL <dariuszpelka88@gazeta.pl>
2019-06-18 08:01:18 +0200

    Implementation of transaction parser

--- MerchantFeeCalculationEngine/Processor/TransactionParser.cs
+++ MerchantFeeCalculationEngine/Processor/TransactionParser.cs
@@ -1,11 +1,28 @@
 ﻿using MerchantFeeCalculationEngine.Model;
+using System;
+using System.Globalization;
+
 namespace MerchantFeeCalculationEngine.Processor
 {
     public class TransactionParser : ITransactionParser
     {
         public Transaction ParseTransactionEntry(string stringifiedTransaction)
         {
-            var transactionList = new Transaction();
+            var stringifiedTransactionDate = stringifiedTransaction.Substring(0, 10);
+            DateTime transactionDate;
+            if (!DateTime.TryParseExact(stringifiedTransactionDate, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out transactionDate))
+            {
+                throw new ArgumentException("Improper format of transaction date");
+            }
+
+            decimal amount;
+            var stringifiedTransactionAmount = stringifiedTransaction.Substring(20, 3);
+            if (!decimal.TryParse(stringifiedTransactionAmount, out amount))
+            {
+                throw new ArgumentException("Improper format of transaction amount");
+            }
+            var merchant = stringifiedTransaction.Substring(11, 8);
+            var transactionList = new Transaction() { Amount = amount, Owner = new Merchant() { Name = merchant}, DoneOn = transactionDate};
             return transactionList;
         }
     }
---
+++ XUnitTestForMerchantFeeCalculationEngineProject/ParserUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using MerchantFeeCalculationEngine.Model;
+using MerchantFeeCalculationEngine.Processor;
+using Xunit;
+
+namespace MerchantFeeCalculationEngineTestProject
+{
+    public class ParserUnitTests
+    {
+        [Fact]
+        public void TransactionParser_ShouldParseTransaction_WhenCorrectDataProvided()
+        {
+            // Arrange
+            var parser = new TransactionParser();
+
+            // Act
+            var transaction = parser.ParseTransactionEntry("2018-09-01 7-ELEVEN 100");
+
+            // Assert
+            Assert.Equal("7-ELEVEN", transaction.Owner.Name);
+            Assert.Equal(new DateTime(2018, 9, 1), transaction.DoneOn);
+            Assert.Equal(100M, transaction.Amount);
+        }
+
+        [Fact]
+        public void MerchantParser_ShouldParseMerchant_WhenCorrectDataProvided()
+        {
+            // Arrange
+            var parser = new MerchantParser();
+
+            // Act
+            var merchant = parser.ParseMerchantEntry("7-ELEVEN 1 10");
+
+            // Assert
+            Assert.Equal("7-ELEVEN", merchant.Name);
+            Assert.Equal(10, merchant.DiscountPercentage);
+            Assert.Equal(1, merchant.FeeAsPercentage);
+        }
+    }
+}
